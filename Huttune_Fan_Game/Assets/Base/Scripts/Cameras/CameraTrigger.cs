add66fdffd651575eb7308ec3b26f16637b7ff69
TaaviM88Laptop <juomakuski@hotmail.com>
2021-01-15 14:51:53 +0200

    peephole system started

---
+++ Huttune_Fan_Game/Assets/Base/Scripts/Cameras/CameraTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrigger : MonoBehaviour
+{
+    public Camera cameraToSwitch;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraToSwitch.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            cameraToSwitch.enabled = true;
+            //other.gameObject.GetComponent<PlayerManager>().playerCamera.enabled = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            cameraToSwitch.enabled = false;
+            //other.gameObject.GetComponent<PlayerManager>().playerCamera.enabled = true;
+        }
+    }
+}
---
+++ Huttune_Fan_Game/Assets/Base/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    [SerializeField]
+    public Camera playerCamera { get; set; }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(Transform eachChild in transform)
+        {
+            playerCamera = eachChild.GetComponent<Camera>();
+            if(playerCamera != null)
+            {
+                break;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
