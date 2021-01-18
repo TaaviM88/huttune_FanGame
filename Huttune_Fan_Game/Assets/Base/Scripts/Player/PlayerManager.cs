b5b5027b9c7cf6c7ebb7aa477c86dc86b0c362d5
Taavi <juomakuski@hotmail.com>
2021-01-19 00:20:04 +0200

    Seksikäs haamu visio

--- Huttune_Fan_Game/Assets/Base/Scripts/Player/PlayerManager.cs
+++ Huttune_Fan_Game/Assets/Base/Scripts/Player/PlayerManager.cs
@@ -9,6 +9,8 @@ public class PlayerManager : MonoBehaviour
 
     public PlayerEnumManager enumManager { get; set; }
 
+    public LayerMask all;
+    public LayerMask normal;
     // Start is called before the first frame update
     void Awake()
     {
@@ -22,11 +24,22 @@ public class PlayerManager : MonoBehaviour
         }
 
         enumManager = GetComponent<PlayerEnumManager>();
+        playerCamera.cullingMask = normal;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Fire1"))
+        {
+            playerCamera.cullingMask = all;
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            playerCamera.cullingMask = normal;
+        }
+
+
     }
 }
---
+++ Huttune_Fan_Game/Assets/Base/Scripts/PostProcessingActivate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.Rendering;
+
+public class PostProcessingActivate : MonoBehaviour
+{
+    Volume volume;
+    private ColorAdjustments colorAdjustments;
+    // Start is called before the first frame update
+    void Start()
+    {
+        volume = GetComponent<Volume>();
+        volume.profile.TryGet(out colorAdjustments);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            colorAdjustments.active = true;
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            colorAdjustments.active = false;
+        }
+
+    }
+}
