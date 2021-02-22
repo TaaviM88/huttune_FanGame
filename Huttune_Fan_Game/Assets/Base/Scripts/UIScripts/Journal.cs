6cdb04198981ca7b0380f026edf120a1b23aa325
TaaviM88Laptop <juomakuski@hotmail.com>
2021-02-22 14:16:08 +0200

    journal script ja sillee niin joo

---
+++ Huttune_Fan_Game/Assets/Base/Scripts/UIScripts/Journal.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Journal : MonoBehaviour
+{
+    public static Journal Instance;
+    public TMP_Text textbox;
+    public float textCooldown = 3f;
+    float countdown = 0;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+        countdown = textCooldown;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if(countdown <= 0f)
+        {
+            textbox.text = "";
+        }
+    }
+
+    public void Log(string newLog)
+    {
+        textbox.text = newLog;
+        countdown = textCooldown;
+    }
+}
