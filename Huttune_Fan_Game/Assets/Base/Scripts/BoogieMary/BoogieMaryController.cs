6bb551e175bf679a7edc5823476df014b8c550a9
TaaviM88Laptop <juomakuski@hotmail.com>
2021-03-01 15:44:29 +0200

    BoogieBoogie Mary AI

--- Huttune_Fan_Game/Assets/Base/Scripts/BoogieMary/BoogieMaryController.cs
+++ Huttune_Fan_Game/Assets/Base/Scripts/BoogieMary/BoogieMaryController.cs
@@ -1,3 +1,5 @@
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,10 +12,15 @@ public class BoogieMaryController : MonoBehaviour
     public Transform playerLocation;
     Transform lastknowLocation;
     Transform cheeseLocation;
-
+    Vector3 target;
     BoogieMaryStoryState storyState = BoogieMaryStoryState.Sleeping;
     BoogieMaryMoveState moveState = BoogieMaryMoveState.None;
     BoogieMaryActionState actionState = BoogieMaryActionState.None;
+
+    bool canMove = true;
+    bool isWalking = false;
+    bool reachedTarget = false;
+    public bool isSleepping = true;
 
     // Start is called before the first frame update
     void Start()
@@ -22,9 +29,92 @@ public class BoogieMaryController : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Upadate()
     {
-        agent.SetDestination(playerLocation.position);
+        if(!canMove)
+        {
+            return;
+        }
     }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!canMove)
+        {
+            return;
+        }
+
+        ProcessRaycast();
+
+        switch (moveState)
+        {
+            case BoogieMaryMoveState.None:
+                target = playerLocation.position;
+                break;
+            case BoogieMaryMoveState.Wandering:
+                break;
+            case BoogieMaryMoveState.ChasingPlayer:
+                break;
+            case BoogieMaryMoveState.ChasingCheese:
+                break;
+            case BoogieMaryMoveState.MoveToTarget:
+                break;
+            default:
+                break;
+        }
+
+        MoveToTarget(target);
+    }
+
+    private void ProcessRaycast()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Vector3.forward, out hit, 5))
+        { 
+                hit.collider.gameObject.GetComponent<IInteractable>()?.Interact();
+                
+        }
+
+        
+    }
+
+    private void MoveToTarget(Vector3 newtarget)
+    {
+        agent.SetDestination(newtarget);
+        transform.LookAt(newtarget, Vector3.up);
+
+        if (agent.pathStatus == NavMeshPathStatus.PathComplete && agent.remainingDistance <= agent.stoppingDistance)
+        {
+            print("Perillä");
+            reachedTarget = true;
+        }
+    }
+
+    public void Attack()
+    {
+
+    }
+
+    public void AwakeMary()
+    {
+
+    }
+
+    public void TriggerAnimation()
+    {
+
+    }
+
+    public void StopMary()
+    {
+
+    }
+
+    public void FlipOver()
+    {
+
+    }
+
 }
